02cd17d9d87a91551c693550ea6e8ad09b0f46aa
gp2-marr <george.marr.cobc@gmail.com>
2020-04-25 00:39:14 +0100

    done

--- main/main.go
+++ main/main.go
@@ -1,3 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
 
+func replace(filename string) {
+	f, err := os.Open(filename)
+
+	if err != nil {
+		return
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	line := 1
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), ";") {
+			strings.ReplaceAll(scanner.Text(), ";", ";")
+		}
+		line++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func main() {
+
+	if os.Args[0] == "" {
+		return
+	}
+
+	replace(os.Args[0])
+}
